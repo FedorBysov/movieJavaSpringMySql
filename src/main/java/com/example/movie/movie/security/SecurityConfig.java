d611f957442966a3d21f5d85833e9374158a68f4
FedorBysov <bysov1987@mail.ru>
2023-12-01 03:23:45 +0300

    .

--- src/main/java/com/example/movie/movie/security/SecurityConfig.java
+++ src/main/java/com/example/movie/movie/security/SecurityConfig.java
@@ -4,13 +4,16 @@ import com.example.movie.movie.security.filter.AuthenticationFilter;
 import com.example.movie.movie.security.filter.AuthorizationFilter;
 import com.example.movie.movie.service.impl.UserDetailServiceImpl;
 import com.example.movie.movie.util.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +22,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
 
@@ -64,8 +69,7 @@ public class SecurityConfig {
         return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
                 .userDetailsService(userDetailService)
                 .passwordEncoder(passwordEncoder)
-                .configure(a)
-                .build();
+                .
 
     }
 
